ac956612374e8bccedbbba2978e7ac16dd037053
Bowab <Bowas92@gmail.com>
2018-10-16 21:44:21 +0200

    Comment added to js file.

--- src/main/resources/static/js/HelloScript.js
+++ src/main/resources/static/js/HelloScript.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
    console.log("Hello from a js script!");
 
-   console.log("A merge test!");
+   //This time with a comment.
+   console.log("Another merge test with comment");
 
     $("#pressMeBtn").on("click", function () {
         console.log("Hello Im btn!");
