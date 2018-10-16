9591607de6a09aa8f3344ccbb4896c3c63a1601b
Bowab <Bowas92@gmail.com>
2018-10-16 22:11:55 +0200

    Added button comment.

--- src/main/resources/static/js/HelloScript.js
+++ src/main/resources/static/js/HelloScript.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
    console.log("Hello from a js script!");
 
-
+    //A test button for later use.
     $("#pressMeBtn").on("click", function () {
         console.log("Hello Im btn!");
     });
