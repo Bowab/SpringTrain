25d17841bb8aa9670df1cb7327d396dbf01b133b
Bowab <Bowas92@gmail.com>
2018-10-16 18:08:40 +0200

    Pretended new feature v2.

--- src/main/resources/static/js/HelloScript.js
+++ src/main/resources/static/js/HelloScript.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
    console.log("Hello from a js script!");
 
 
-    console.log("This is a pretended new feature.");
+    console.log("This is a pretended new feature!!!!");
 
 
     $("#pressMeBtn").on("click", function () {
