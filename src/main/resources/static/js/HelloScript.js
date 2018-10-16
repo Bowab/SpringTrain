e2da3925d49dcda99880a8ceee141b253e01046a
Bowab <Bowas92@gmail.com>
2018-10-16 21:36:17 +0200

    Random-merge testing.

--- src/main/resources/static/js/HelloScript.js
+++ src/main/resources/static/js/HelloScript.js
@@ -1,10 +1,7 @@
 $(document).ready(function () {
    console.log("Hello from a js script!");
 
-   console.log("Last merge-test");
-   console.log("Last merge-test");
-   console.log("Last merge-test");
-
+   console.log("A merge test!");
 
     $("#pressMeBtn").on("click", function () {
         console.log("Hello Im btn!");
