3c65376a00e3e7100bef562ff98f693cd90ab912
Bowab <Bowas92@gmail.com>
2018-10-16 21:55:34 +0200

    HelloScript cleanup after merge-testing.

--- src/main/resources/static/js/HelloScript.js
+++ src/main/resources/static/js/HelloScript.js
@@ -1,14 +1,6 @@
 $(document).ready(function () {
    console.log("Hello from a js script!");
 
-   //This time with a comment.
-   console.log("Another merge test with comment");
-
-   /*
-
-   adding a comment in this block!
-
-    */
 
     $("#pressMeBtn").on("click", function () {
         console.log("Hello Im btn!");
