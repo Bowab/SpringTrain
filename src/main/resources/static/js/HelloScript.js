740d2b2b5d54299837c4435feb9a79dc1a5a15c5
Bowab <Bowas92@gmail.com>
2018-10-16 17:47:06 +0200

    Test branch commit.

--- src/main/resources/static/js/HelloScript.js
+++ src/main/resources/static/js/HelloScript.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
    console.log("Hello from a js script!");
 
 
-    console.log("This is a test!");
+    console.log("This is a test!!!");
 
 
     $("#pressMeBtn").on("click", function () {
