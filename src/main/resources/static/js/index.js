4c7990c77a91e995f4448fb40183b3eba6e68e57
Bowab <Bowas92@gmail.com>
2018-10-17 21:06:50 +0200

    Renaming and removing of css files.

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    console.log("Hello from a js script!");
+    console.log("Hello from a js script!");
+    console.log("Hello from a js script!");
+    console.log("Hello from a js script!");
+    console.log("Hello from a js script!");
+    console.log("Hello from a js script!");
+
+    //A test button for later use.
+    $("#pressMeBtn").on("click", function () {
+        console.log("Hello Im btn!");
+    });
+
+});
+
+
