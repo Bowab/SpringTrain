98efd2dbff922f176eca8ae329eada46d92d88f2
Bowab <Bowas92@gmail.com>
2018-11-01 10:09:23 +0100

    Remove test commit changes.

--- src/main/java/com/main/profiletower/controllers/HomeController.java
+++ src/main/java/com/main/profiletower/controllers/HomeController.java
@@ -28,9 +28,6 @@ public class HomeController {
     @Autowired
     private MessageService messageService;
 
-    //Delete this
-    @Autowired
-    private MessageService messageService2;
 
     @Autowired
     private UserService userService;
